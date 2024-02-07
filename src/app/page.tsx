e38f64726d599b5dfe723296515e9090dc124aba
becallgit <cuellarjotis@gmail.com>
2024-02-07 15:18:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
-import { BackendProvider } from "@/context";
-import { Item, Cart } from "@/components";
-
-import { stripe } from "@/lib"
-
-async function getPrices() {
-
-  const prices = await stripe?.prices.list();
-
-  if (!prices) {
-    throw new Error("Failed to fetch prices");
-  }
-
-  return prices.data;
-
-}
-
 export default async function Home() {
-
-  const prices = await getPrices();
 
   return (
     <main>
-      <BackendProvider>
-        <Cart />
-        {prices.map((price) => (
-          <Item key={price.id} {...price} />
-        ))}
-      </BackendProvider>
     </main>
   );
 }
